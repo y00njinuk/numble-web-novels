7d6da846a7e3a2fca43a3a91d0411c4472b8b749
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-04-05 21:46:04 +0900

    Init

---
+++ src/main/java/com/numble/webnovels/NumbleWebNovelsApplication.java
@@ -0,0 +1,13 @@
+package com.numble.webnovels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleWebNovelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumbleWebNovelsApplication.class, args);
+    }
+
+}
