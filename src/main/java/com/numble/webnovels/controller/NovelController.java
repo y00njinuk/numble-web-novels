be156a9bc04393f015344a76703ed7d13883c904
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-04-09 22:27:50 +0900

    feat: add NovelController and integration test using MockMVC
    - 컨트롤러 정의 및 통합 테스트 구현

---
+++ src/main/java/com/numble/webnovels/controller/NovelController.java
@@ -0,0 +1,59 @@
+package com.numble.webnovels.controller;
+
+import com.numble.webnovels.dto.NovelDetailResponseDto;
+import com.numble.webnovels.dto.NovelResponseDto;
+import com.numble.webnovels.service.NovelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/novel")
+public class NovelController {
+
+    private final NovelService novelService;
+
+    @Autowired
+    public NovelController(NovelService novelService) {
+        this.novelService = novelService;
+    }
+
+    @GetMapping("/search/best")
+    public ResponseEntity<List<NovelResponseDto>> getAllBestNovels() {
+        List<NovelResponseDto> novelResponseDtoList = novelService.getAllBestNovels();
+
+        // TODO. 첫 번째 소설(편당) 조회를 통해 파일 경로를 찾고 이미지 불러오기
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(novelResponseDtoList);
+    }
+
+    @GetMapping("/search/topic")
+    public ResponseEntity<NovelResponseDto> getNewBestNovel() {
+        NovelResponseDto novelResponseDto = novelService.getNewBestNovel();
+
+        // TODO. 첫 번째 소설(편당) 조회를 통해 파일 경로를 찾고 이미지 불러오기
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(novelResponseDto);
+    }
+
+    @GetMapping("/search/{novelId}")
+    public ResponseEntity<NovelDetailResponseDto> getNovelById(@PathVariable Long novelId) {
+        NovelDetailResponseDto novelResponseDto = novelService.getNovel(novelId);
+
+        // TODO. 첫 번째 소설(편당) 조회를 통해 파일 경로를 찾고 이미지 불러오기
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(novelResponseDto);
+    }
+}
---
+++ src/test/java/com/numble/webnovels/NumbleWebNovelsIntegrationTest.java
@@ -0,0 +1,99 @@
+/**
+ * Spring Boot Integration Test
+ *
+ * Reference: spring.io/guides/gs/testing-web/#:~:text=Another%20useful%20approach,starting%20the%20server.
+ */
+
+package com.numble.webnovels;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.numble.webnovels.domain.Novel;
+import com.numble.webnovels.domain.NovelItem;
+import com.numble.webnovels.repository.NovelItemRepository;
+import com.numble.webnovels.repository.NovelRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class NumbleWebNovelsIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    NovelRepository novelRepository;
+
+    @Autowired
+    NovelItemRepository novelItemRepository;
+
+    @BeforeEach
+    void setUp() {
+        // not implemented.
+    }
+
+    @AfterEach
+    void tearDown() {
+        novelRepository.deleteAll();
+        novelItemRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("소설 ID 값을 통해 소설을 정상적으로 조회할 수 있다.")
+    void givenNovelId_whenFindById_thenReturnNovel() throws Exception {
+        Random rndGen = new Random();
+
+        List<NovelItem> novelItemList =
+                IntStream
+                        .iterate(1, i -> i + 1)
+                        .limit(rndGen.nextInt(10) + 1)
+                        .mapToObj(n -> new NovelItem(
+                                new Long(n),
+                                rndGen.nextInt(100),
+                                rndGen.nextInt(10),
+                                rndGen.nextBoolean(),
+                                rndGen.nextInt(1024),
+                                "src/main/resources/sample/novel1-" + n))
+                        .collect(Collectors.toList());
+
+        Novel newNovel = new Novel(1L, "Numble Novels", "Nuble Novels", 0,
+                new ArrayList<>());
+
+        newNovel.addNovelItem(novelItemList);
+        novelRepository.saveAndFlush(newNovel);
+
+        mockMvc.perform(get("/api/novel/search/"+ newNovel.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.title").value(newNovel.getTitle()))
+                .andExpect(jsonPath("$.comment").value(newNovel.getComment()))
+                .andExpect(jsonPath("$.clickCount").value(newNovel.getClickCount()))
+                .andExpect(jsonPath("$.novelItemList").isArray())
+                // TODO. Json 배열에 있는 원소 비교 구문 추가
+                .andExpect(jsonPath("$.novelItemList", hasSize(novelItemList.size())));
+    }
+}
